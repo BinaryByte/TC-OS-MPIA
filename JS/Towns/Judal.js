9947905f32329044e7f00c627eb30abafdaa3804
Tyly04 <tyly04@gmail.com>
2016-04-20 12:11:25 -0700

    Update Judal.js

--- JS/Towns/Judal.js
+++ JS/Towns/Judal.js
@@ -1,4 +1,5 @@
 function arriveJudal() {
+    //Presents you with the choice to leave.
     clear();
     document.innerHTML = "You are now in the city of Judal.";
     document.innerHTML = document.innerHTML + "<p><button onClick = \"leaveJudal()\">Leave</button></p>";
