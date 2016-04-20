1b4e03feefc0ea583a7724f936666710407d711a
Tyly04 <tyly04@gmail.com>
2016-04-20 12:11:06 -0700

    Added comments

--- JS/Towns/Nok.js
+++ JS/Towns/Nok.js
@@ -1,5 +1,6 @@
 function arriveNok() {
     if (storyLineBegan === true) {
+        //If the storyline started, executes storylineDemon() in Story.js
         clear();
         document.innerHTML = "There\'s lots of fire everywhere. A guard lets you into the gates.";
         document.innerHTML = document.innerHTML + "<p>The city of Nok is in flames. A burning building rumbles and shakes, with screams coming from inside. A demon emerges, flattening the building, and it swings a mighty claw...</p>";
@@ -12,12 +13,14 @@ function arriveNok() {
 };
 
 function arriveNokBurned() {
+    //Arrive at burned down nok
     clear();
     document.innerHTML = "You arrive at the burned city of Nok.";
     document.innerHTML = document.innerHTML + "<p><button onClick =\"nokLeave()\">Leave</button></p>";
 };
 
 function nokLeave() {
+    //Executes travel code in Travel.js
     clear();
     document.innerHTML = document.innerHTML + "<button onClick = \"travelJudalSet()\">Judal</button><button onClick = \"travelArgoSet()\">Argo</button><button onClick = \"arriveNokBurned()\">Stay</button>";
 };
