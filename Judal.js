5fbe331a48616da6bbe1ade4be7522bbb3d3a329
Tyly04 <tyly04@gmail.com>
2016-04-20 09:05:10 -0700

    Update Judal.js

--- Judal.js
+++ Judal.js
@@ -1,1 +1,5 @@
-
+function arriveJudal() {
+    clear();
+    document.write("You are now in the city of Judal.");
+    document.write("<p><button onClick = \"leaveJudal()\">Leave</button></p>");
+};
