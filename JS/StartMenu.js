989434db1f0dd16e0315db97ada2089c9c0d55a1
Tyly04 <tyly04@gmail.com>
2016-04-20 10:25:58 -0700

    Fixed window.alert statements

--- JS/StartMenu.js
+++ JS/StartMenu.js
@@ -8,13 +8,13 @@ function difficultySet() {
             defense = 14;
             health = 20;
             maxHealth = 20;
-            alert("Difficulty set to easy.");
+            window.alert("Difficulty set to easy.");
             break;
         case 'Medium':
-            alert("Difficulty set to medium.");
+            window.alert("Difficulty set to medium.");
             break;
         case 'Hard':
-            alert("Difficulty set to Hard.");
+            window.alert("Difficulty set to Hard.");
             gold = 7;
             attack = 7;
             defense = 7;
@@ -22,7 +22,7 @@ function difficultySet() {
             maxHealth = 7;
             break;
         case 'Insane':
-            alert("Difficulty set to Insane.");
+            window.alert("Difficulty set to Insane.");
             gold = 5;
             attack = 5;
             defense = 5;
@@ -30,7 +30,7 @@ function difficultySet() {
             maxHealth = 5;
             break;
         default:
-            alert("I'm sorry, I couldn't recognize that. Please type it exactly as spelled, and it's case sensitive, so type it with the exact capital letters.");
+            window.alert("I'm sorry, I couldn't recognize that. Please type it exactly as spelled, and it's case sensitive, so type it with the exact capital letters.");
             break;
     }
 }
@@ -38,13 +38,13 @@ function difficultySet() {
 function options() {
     clicks = clicks + 1;
     if (clicks >= 10) {
-        alert("Wow. You really want an options menu. Well TOO BAD! This is a text adventure, not some panzie FPS. This, is for hardcore gamers, who don't need \"Configurations\" or \"Gaming PCS\". This is FOR REAL MEN... or women. Gender Equality. YOU WILL BE DRILLED TO YOUR CORE, HEADING INTO THE DARK UNKOWN, WITH ONLY YOUR WITS AND AN INVENTORY TO GUIDE YOU. YOU WILL SURVIVE. YOU WILL... Well, you've been eaten by a grue. Goodbye.");
+        window.alert("Wow. You really want an options menu. Well TOO BAD! This is a text adventure, not some panzie FPS. This, is for hardcore gamers, who don't need \"Configurations\" or \"Gaming PCS\". This is FOR REAL MEN... or women. Gender Equality. YOU WILL BE DRILLED TO YOUR CORE, HEADING INTO THE DARK UNKOWN, WITH ONLY YOUR WITS AND AN INVENTORY TO GUIDE YOU. YOU WILL SURVIVE. YOU WILL... Well, you've been eaten by a grue. Goodbye.");
     } else {
         var fov = prompt("Set your FOV. Pick a number between -1000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000 and 10000000000000000000000000000000000000000000000000000000000000000000000000000000");
-        alert("This is a text adventure game, so I will disable the FOV. I do admire your courage, for picking " + fov + ".");
+        window.alert("This is a text adventure game, so I will disable the FOV. I do admire your courage, for picking " + fov + ".");
     }
 }
 
 function evil() {
-    alert("Yes, I am evil.");
+    window.alert("Yes, I am evil.");
 };
