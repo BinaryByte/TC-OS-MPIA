ec8564200a2143b62e6f3a705fae87cb08513f5e
Tyly04 <tyly04@gmail.com>
2016-04-20 12:06:00 -0700

    Added comments

--- JS/Story.js
+++ JS/Story.js
@@ -1,17 +1,21 @@
 function storylineStart() {
+    //Detects if during travel you picked up the note (The storyline began variable). If so, then it begins takes you to travelNok.
     if (storyLineBegan === true) {
         travelNok();
     } else {
+        //If during travel you did not pick up the note, it runs the event.
         clear();
         document.innerHTML = "You find an old man running towards Nok with a letter in his hand. He collapses. You feel compelled to open the note. <p><button onClick = \"pickNote()\">Pick Up Note</button><button onClick = \"butt()\")\">Don't Pick Up Note</button></p>";
     }
 };
 
 function butt() {
+    //Forces you to pick up the note.
     document.innerHTML = document.innerHTML + "<p>A butthurt developer wants you to go pick up the note.</p>";
 };
 
 function pickNote() {
+    //Says that the storyline begins.
     clear();
     distance = distance - travelingSpeed;
     storyLineBegan = true;
@@ -19,19 +23,25 @@ function pickNote() {
 };
 
 function storylineDemon() {
+    //You are attacked by a demon.
     nokBurned = true;
     window.alert("You are attacked by a demon!");
+    //If you have wizard magic, it gives you a bonus.
     if (wizardMagic === true) {
+        //Sets a variable called originalAttack, which is called when combat is over.
         originalAttack = attack;
         window.alert("You are magic! + 30% attack against demons!");
         attack = attack + (attack * 0.3);
     }
+    //Declaring enemy variables.
     enemyHealth = 16;
     enemyDefense = 9;
     enemyAttack = 13;
     var attacked = true;
+    //Sets a while loop that while attacked is true, it will execute the code inside.
     while (attacked === true) {
         if (enemyHealth <= 0) {
+            //Detects if enemy health is less than or equal to zero, and if true, it will end the while loop.
             attacked = false;
             window.alert('You win! + 10 gold!');
             gold = gold + 10;
@@ -40,6 +50,7 @@ function storylineDemon() {
             }
             continueStory();
         } else if (health <= 0) {
+            //Detects if your health is less than or equal to zero, and if true, it will end the while loop.
             health = 10;
             gold = 10;
             attack = 10;
@@ -49,12 +60,16 @@ function storylineDemon() {
             window.alert("You are dead.");
             death();
         } else {
+            //If neither your health or the enemy health is equal to or less then zero, it will execute this code.
+            //Rolls a critical chance, which adds to your attack.
             criticalChance = Math.floor(Math.random() * 5);
+            //Add your attack plus critical chance minus the enemy defense.
             playerAttack = attack + criticalChance - enemyDefense;
             enemyHealth = enemyHealth - playerAttack;
             window.alert("You did " + playerAttack + " damage!");
             window.alert("The enemy health is now " + enemyHealth + "!");
             if (enemyHealth <= 0) {
+                //If the enemy health is less than or equal to zero, it ends the while loop.
                 attacked = false;
                 window.alert('You win!');
                 if (health > maxHealth) {
@@ -65,9 +80,11 @@ function storylineDemon() {
                 }
                 continueStory();
             } else {
+                //Else, it does the same thing that the player did, except for the enemy.
                 criticalChance = Math.floor(Math.random() * 5);
                 enemyAttack2 = (enemyAttack + criticalChance) - defense;
                 if (enemyAttack2 < 0) {
+                    //If the enemy attack is less than zero, it will not add to your health.
                     window.alert("The enemy attack did nothing...");
                 } else {
                     health = health - enemyAttack2;
@@ -75,8 +92,10 @@ function storylineDemon() {
                     window.alert("You are now at " + health + " hit points!");
                 }
                 if (health > maxHealth) {
+                    //If you go over your max health, it resets your health.
                     health = maxHealth;
                     alert("You went over " + maxHealth + "! Resetting health...");
+                    //Repeat the loop.
                 }
             }
         }
@@ -84,18 +103,22 @@ function storylineDemon() {
 };
 
 function continueStory() {
+    //Called once the while loop completes.
     clear();
     document.innerHTML = "You defeat the demon. For being so large, it was surprisingly easy. <p>Over the horizon, however, a larger demon flies towards you. It\'s nearly four times the size of the large demon. However, something pops up and flies it into the ground.</p>";
     document.innerHTML = document.innerHTML + "<p>The thing that brought down the demon comes up, and you see it is a magnificent dragon.</p> <p>The dragon approaches you, and lands directly near you.</p> <p>\"Greetings adventurer. You look worn down. Quickly, you must help me fight off the demon-spawn.\"</p>";
+    //Presented a choice to either help or avoid the dragon
     document.innerHTML = document.innerHTML + "<button onClick = \"helpDragon()\">\"Let\'s go kick some generic bad guy batootie.\"</button><button onClick = \"noDragon()\">\"No! You\'re a dragon!\"</button>";
 };
 
 function helpDragon() {
+    //Dragon heals you.
     clear();
     document.innerHTML = "The Dragon inhales... <p><button onClick =\"helpDragonContinue()\">Ohcrapohcrapocrapocrap</button><button onClick = \"helpDragonContinue()\">Whatever...</button></p>";
 };
 
 function helpDragonContinue() {
+    //Sets a distance of 100, and has you wander with the streetWander() function.
     distance = 100;
     document.innerHTML = document.innerHTML + "<p>And exhales a mighty healing breath. Your health is restored.</p>";
     health = maxHealth;
@@ -104,18 +127,21 @@ function helpDragonContinue() {
 };
 
 function streetWander() {
+    //The saved variable is when you save a woman or not.
     saved = false;
     clear();
     document.innerHTML = "You are wandering down the street, looking for demons and people to save.";
     document.innerHTML = document.innerHTML + "Mandatory encounter in... " + distance / travelingSpeed + "  clicks.";
     document.innerHTML = document.innerHTML + "<p><button onClick = \"streetWanderEvent()\">Wander</button></p>";
     if (distance <= 0) {
-        document.innerHTML = document.innerHTML + "<p>You discover a burning building, and a woman screaming.</p>";
+        //Once the distance is zero, you save a woman from a burning building.
+        document.innerHTML = "<p>You discover a burning building, and a woman screaming.</p>";
         document.innerHTML = document.innerHTML + "<p><button onClick = \"continueOnward()\">She\'s dead anyway.</button><button onClick = \"saveWoman()\">Incognito adventurer to the rescue!</button></p>";
     }
 };
 
 function streetWanderEvent() {
+    //Randomly generates a number, and if that number is less than or equal to 50, it will have a demon attack you.
     streetEvent = Math.floor(Math.random() * 100)
     if (streetEvent > 50) {
         distance = distance - travelingSpeed;
@@ -127,6 +153,7 @@ function streetWanderEvent() {
 };
 
 function demonAttack() {
+    //See storyLineDemon() on line 25 of Story.js for an explanation of combat.
     window.alert("You are attacked by a demon!");
     if (wizardMagic === true) {
         originalAttack = attack;
@@ -191,18 +218,21 @@ function demonAttack() {
 };
 
 function saveWoman() {
+    //sets saved to true, and you save the woman.
     clear();
     saved = true;
     document.innerHTML = "You save the woman. She runs away, like a coward. <p><button onClick = \"continueOnward()\">Let\'s go save more people!</button></p>";
 }
 
 function continueOnward() {
+    //You continue, and are offered the chance to ride on the dragon's back.
     clear();
     document.innerHTML = "You continue, and the dragon meets up with you. He hurriedly speaks. \"There are too many demons blah blah blah blah not just a regular blah blah blah blah hop on my back.\"";
     document.innerHTML = document.innerHTML + "<p><button onClick = \"dragonRide()\">Be awesome and ride a CENSORED dragon!</button><button onClick = \"noDragonRide()\">\"I get uh... dragon... sick...\"</button></p>";
 }
 
 function noDragonRide() {
+    //Sets the destination and exeutes judalNoDragon() function, adding the woman and reducing your travel speed if you saved her.
     clear();
     destination = 'JudalStory'
     document.innerHTML = "\"Whatever, adventurer. I was planning to eat you anyway.\" You begin walking to Judal.";
@@ -217,6 +247,7 @@ function noDragonRide() {
 };
 
 function dragonRide() {
+    //You ride the dragon and fly to Judal. If you saved the woman, she hops on with you.
     distance = 20;
     clear();
     document.innerHTML = "You hop onto the dragon.";
@@ -227,6 +258,7 @@ function dragonRide() {
 };
 
 function dragonFly() {
+    //You fly on the dragon, and it executes code in flyEvent.
     clear();
     dragonFlyS = true;
     document.innerHTML = "Distance to Judal, " + distance + " miles. Dragon speed is 5 miles per minute.";
@@ -237,6 +269,7 @@ function dragonFly() {
 };
 
 function flyEvent() {
+    //If flyChance is less than or equal to 500, it calls demonAttackFly()
     flyChance = Math.floor(Math.random() * 1000);
     if (flyChance > 500) {
         distance = distance - 5;
@@ -253,6 +286,7 @@ function flyEvent() {
 };
 
 function demonAttackFly() {
+    //You don't need to understand this code, we won't be using it later. Yet.
     clear();
     document.innerHTML = "<p>A flying demon attacks you!</p> <p><button onClick = \"dragonLand()\">Combat</button>";
 }
@@ -320,20 +354,17 @@ function dragonDemonAttack() {
 }
 
 function noDragon() {
+    //Having you walk towards the city of Judal, and executes travelNok in Travel.js
     destination = 'JudalStory';
     distance = 20;
     clear();
     document.innerHTML = "The dragon flies off. You walk towards the city of Judal.";
-    if (saved === true) {
-        travelingSpeedOriginal = travelingSpeed;
-        travelingSpeed = travelingSpeed - 5;
-        document.innerHTML = document.innerHTML + "<p>You have saved the woman, so you get a deduction to your traveling speed. - 5 traveling speed. Your total traveling speed is now " + travelingSpeed + " miles per day.</p>";
-    }
     travelNok();
 };
 
 
 function arriveJudalStory() {
+    //Executes the code for arriving at judal, and restores your health.
     clear();
         document.innerHTML = "You arrive at Judal.";
     document.innerHTML = "<p>The mayor of Judal greets you and offers you gold for killing demons. + 50 gold.</p>";
@@ -348,6 +379,7 @@ function arriveJudalStory() {
 };
 
 function judalStoryBanquet() {
+    //Detects which class you are, and gives you a gift depending on that class.
     clear();
     document.innerHTML = "You eat, and your health is restored. You are now at " + health + " hit points. <p>The mayor then presents you with a gift, to help discover the cause of these demon incursions.</p>";
     if (backstory === 'Wizard') {
