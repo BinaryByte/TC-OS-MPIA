f3d236054018805528555b03da204c473cf9cb8b
Tyly04 <tyly04@gmail.com>
2016-04-20 11:51:41 -0700

    Added comments

--- JS/StartMenu.js
+++ JS/StartMenu.js
@@ -1,7 +1,9 @@
 //Contains stuff for the start menu
 function difficultySet() {
+    //Prompts you for the difficulty
     var difficulty = prompt("Easy, Medium, Hard, or Insane?");
     switch (difficulty) {
+        //Sets the difficulty based on your response
         case 'Easy':
             gold = 20;
             attack = 14;
@@ -30,12 +32,14 @@ function difficultySet() {
             maxHealth = 5;
             break;
         default:
+        //If not recognized, it tells you that it couldn't recognize your string.
             window.alert("I'm sorry, I couldn't recognize that. Please type it exactly as spelled, and it's case sensitive, so type it with the exact capital letters.");
             break;
     }
 }
 
 function options() {
+    //Opens the options menu, and detects if you've clicked more than ten times.
     clicks = clicks + 1;
     if (clicks >= 10) {
         window.alert("Wow. You really want an options menu. Well TOO BAD! This is a text adventure, not some panzie FPS. This, is for hardcore gamers, who don't need \"Configurations\" or \"Gaming PCS\". This is FOR REAL MEN... or women. Gender Equality. YOU WILL BE DRILLED TO YOUR CORE, HEADING INTO THE DARK UNKOWN, WITH ONLY YOUR WITS AND AN INVENTORY TO GUIDE YOU. YOU WILL SURVIVE. YOU WILL... Well, you've been eaten by a grue. Goodbye.");
@@ -46,5 +50,6 @@ function options() {
 }
 
 function evil() {
+    //This is called in TC-OSMPIA.html, where when you click on the text with Comic Sans MS, it displays this.
     window.alert("Yes, I am evil.");
 };
