e9849009b2385f6d40698f60b021fee1c43e0cce
Tyly04 <tyly04@gmail.com>
2016-04-21 16:32:54 -0700

    Added Explore Button

--- JS/Towns/Judal.js
+++ JS/Towns/Judal.js
@@ -2,7 +2,7 @@ function arriveJudal() {
     //Presents you with the choice to leave.
     clear();
     document.body.innerHTML = "You are now in the city of Judal.";
-    document.body.innerHTML = document.body.innerHTML + "<p><button onClick = \"judalStore()\">Shop</button><button onClick = \"leaveJudal()\">Leave</button></p>";
+    document.body.innerHTML = document.body.innerHTML + "<p><button onClick = \"\">Explore \(In development\)</button><button onClick = \"judalStore()\">Shop</button><button onClick = \"leaveJudal()\">Leave</button></p>";
 };
 //<input value=\"\" type=\"button\" onClick=\"\"></input> button template
 
