a89a781fa442c8edcb562de5b8ce9eed34511a61
Tyly04 <tyly04@gmail.com>
2016-04-21 11:00:48 -0700

    Added document.body.innerHTML

--- JS/Towns/Argo.js
+++ JS/Towns/Argo.js
@@ -1,22 +1,22 @@
 
 function again() {
     //Presents you choices to do stuff in argo.
-    document.innerHTML = document.innerHTML + "<p>You are in the town of Argo.</p>";
-    document.innerHTML = document.innerHTML + "<button onClick = \"exploreArgo()\">Explore</button><button onClick = \"shopArgo()\">Shop</button><button onClick = \"leaveArgo()\">Travel to Nok</button>";
+    document.body.innerHTML = document.body.innerHTML + "<p>You are in the town of Argo.</p>";
+    document.body.innerHTML = document.body.innerHTML + "<button onClick = \"exploreArgo()\">Explore</button><button onClick = \"shopArgo()\">Shop</button><button onClick = \"leaveArgo()\">Travel to Nok</button>";
 };
 
 
 function leaveArgo() {
     //Presents you with the leave choice.
-    document.innerHTML = "You leave Argo.";
+    document.body.innerHTML = "You leave Argo.";
     if (nokBurned === true) {
-        document.innerHTML = document.innerHTML + "<p><button onClick = \"travelJudalSet()\">Judal</button><button onClick = \"travelNokSet()\">Nok</button><button onClick = \"again()\">Stay</button></p>";
+        document.body.innerHTML = document.body.innerHTML + "<p><button onClick = \"travelJudalSet()\">Judal</button><button onClick = \"travelNokSet()\">Nok</button><button onClick = \"again()\">Stay</button></p>";
     } else {
         distance = distanceToNok;
         destination = 'Nok';
         clear();
-        document.innerHTML = "<p>The only nearby town is the town of Nok. Do you wish to travel there?</p>";
-        document.innerHTML = document.innerHTML + "<p><button onClick = \"travelNok()\">Yes</button><button onClick = \"again()\">No</button></p>";
+        document.body.innerHTML = "<p>The only nearby town is the town of Nok. Do you wish to travel there?</p>";
+        document.body.innerHTML = document.body.innerHTML + "<p><button onClick = \"travelNok()\">Yes</button><button onClick = \"again()\">No</button></p>";
     }
 };
 
@@ -25,27 +25,27 @@ function exploreArgo() {
     //If you decide to explore, you are given the choice to go to an alley.
     if (ambushArgoT === true) {
         //If you've already been ambushed, it tells you there's nothing there.
-        document.innerHTML = document.innerHTML + "<p>You\'ve already explored the alley.</p>";
+        document.body.innerHTML = document.body.innerHTML + "<p>You\'ve already explored the alley.</p>";
     } else if (ambushArgoT === false) {
         clear();
         //If you haven't been ambushed or left the alley but decide to come back, then it loads the same choice as above.
-        document.innerHTML = "You find a small alley.";
-        document.innerHTML = document.innerHTML + "<p><button onClick = \"ambushArgo()\">Explore Alley</button><button onClick = \"again()\">Leave the alley</button></p>";
+        document.body.innerHTML = "You find a small alley.";
+        document.body.innerHTML = document.body.innerHTML + "<p><button onClick = \"ambushArgo()\">Explore Alley</button><button onClick = \"again()\">Leave the alley</button></p>";
     }
 }
 
 function shopArgo() {
     //A shop with materials, that have buttons that allow you to upgrade your sword.
     clear();
-    document.innerHTML = "You have " + gold + " gold. What would you like to purchase?";
-    document.innerHTML = document.innerHTML + "<p><button onClick = \"armorArgo()\">Armor $20</button><button onClick = \"swordArgo()\">Sword $20</button><button onClick = \"healthArgo()\">Restore Health $10</button><button onClick = \"again()\">Leave</button></p>";
+    document.body.innerHTML = "You have " + gold + " gold. What would you like to purchase?";
+    document.body.innerHTML = document.body.innerHTML + "<p><button onClick = \"armorArgo()\">Armor $20</button><button onClick = \"swordArgo()\">Sword $20</button><button onClick = \"healthArgo()\">Restore Health $10</button><button onClick = \"again()\">Leave</button></p>";
 };
 
 function swordArgo() {
     //Sword purchasing.
     if (swordPurchase === false) {
         if (gold < 20) {
-            document.innerHTML = document.innerHTML + "You don't have enough.";
+            document.body.innerHTML = document.body.innerHTML + "You don't have enough.";
             again();
         } else {
             window.alert("You purchased a sword! + 3 attack!");
@@ -56,7 +56,7 @@ function swordArgo() {
         }
     } else if (!swordPurchase) {
         if (gold < 20) {
-            document.innerHTML = document.innerHTML + "<p>You don't have enough.</p>";
+            document.body.innerHTML = document.body.innerHTML + "<p>You don't have enough.</p>";
             again();
         } else {
             window.alert("You purchased a sword! + 3 attack!");
@@ -66,7 +66,7 @@ function swordArgo() {
             swordPurchase = true;
         }
     } else {
-        document.innerHTML = document.innerHTML + "You already purchased a sword.";
+        document.body.innerHTML = document.body.innerHTML + "You already purchased a sword.";
         again();
     }
 };
@@ -75,7 +75,7 @@ function armorArgo() {
     //armor purchasing
     if (armorPurchase === false) {
         if (gold < 20) {
-            document.innerHTML = document.innerHTML + "You can't afford that!";
+            document.body.innerHTML = document.body.innerHTML + "You can't afford that!";
             again();
         } else {
             gold = gold - 20;
@@ -86,7 +86,7 @@ function armorArgo() {
         }
     } else if (!armorPurchase) {
         if (gold < 20) {
-            document.innerHTML = document.innerHTML  + "You can't afford that!";
+            document.body.innerHTML = document.body.innerHTML  + "You can't afford that!";
             again();
         } else {
             gold = gold - 20;
@@ -96,7 +96,7 @@ function armorArgo() {
             armorPurchase = true;
         }
     } else {
-        document.innerHTML = document.innerHTML + "<p>You've already purchased armor!</p>";
+        document.body.innerHTML = document.body.innerHTML + "<p>You've already purchased armor!</p>";
         again();
     }
 };
@@ -104,16 +104,16 @@ function armorArgo() {
 function healthArgo() {
     //restores health
     if (maxHealth === health) {
-        document.innerHTML = document.innerHTML + "<p>You're already at full health!</p>";
+        document.body.innerHTML = document.body.innerHTML + "<p>You're already at full health!</p>";
         again();
     } else {
         if (gold < 10) {
-            document.innerHTML = document.innerHTML + "<p>You can't afford that!</p>";
+            document.body.innerHTML = document.body.innerHTML + "<p>You can't afford that!</p>";
             again();
         } else {
             gold = gold - 10;
             health = maxHealth;
-            document.innerHTML = document.innerHTML + "<p>Health restored!</p>";
+            document.body.innerHTML = document.body.innerHTML + "<p>Health restored!</p>";
             again();
         }
     }
@@ -123,7 +123,7 @@ function healthArgo() {
 function ambushArgo() {
     //Attacked. For a full rundown of combat code, see Line 25 of Story.js
     clear();
-    document.innerHTML = "You are attacked by thugs!";
+    document.body.innerHTML = "You are attacked by thugs!";
     var attacked = true;
     enemyHealth = 5;
     enemyDefense = 5;
