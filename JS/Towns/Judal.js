82df24829b6c01bb0c01b96eb264a5f0b09c4bf8
Tyly04 <tyly04@gmail.com>
2016-04-21 11:01:17 -0700

    Added document.body.innerHTML

--- JS/Towns/Judal.js
+++ JS/Towns/Judal.js
@@ -1,6 +1,6 @@
 function arriveJudal() {
     //Presents you with the choice to leave.
     clear();
-    document.innerHTML = "You are now in the city of Judal.";
-    document.innerHTML = document.innerHTML + "<p><button onClick = \"leaveJudal()\">Leave</button></p>";
+    document.body.innerHTML = "You are now in the city of Judal.";
+    document.body.innerHTML = document.body.innerHTML + "<p><button onClick = \"leaveJudal()\">Leave</button></p>";
 };
