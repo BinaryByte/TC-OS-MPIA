da75e835682f9b3e2aa511358253ec5235b18c7c
Nis <niscooln@gmail.com>
2016-04-22 13:42:01 -0700

    updated spacing

--- JS/Towns/Nok.js
+++ JS/Towns/Nok.js
@@ -8,7 +8,7 @@ function arriveNok() {
     } else if (storyLineBegan === false) {
         clear();
         document.body.innerHTML = "You are in the town of Nok. The gates are barred. You must have missed a plot device.";
-        document.body.innerHTML = document.body.innerHTML + "<p><button onClick = \"travelArgo()\">Go to Argo</button></p>";
+        document.body.innerHTML = document.body.innerHTML + "<p><button onClick = \"travelArgo()\"> Go to Argo </button></p>";
     }
 };
 
@@ -16,20 +16,20 @@ function arriveNokBurned() {
     //Arrive at burned down nok
     clear();
     document.body.innerHTML = "You arrive at the burned city of Nok, there is a guard at the gate, he says that a magical construction team from the future arrived a few days ago, and is busy turning it into a 'target' whatever that is. They say they'll be done in about 4 years. You can still enter the story though.";
-    document.body.innerHTML = document.body.innerHTML + "<p><button onClick =\"nokLeave()\">Leave</button><button onClick = \"nokEnter()\">Enter the City</button></p>";
+    document.body.innerHTML = document.body.innerHTML + "<p><button onClick =\"nokLeave()\"> Leave </button><button onClick = \"nokEnter()\"> Enter the City </button></p>";
 };
 
 function nokEnter(){
-    document.body.innerHTML = "You are now in the city of Nok. <button onClick = \"arriveNokBurned()\">Leave the city</button><button onClick = \"construction()\">Walk to Construction Site</button>";
+    document.body.innerHTML = "You are now in the city of Nok. <button onClick = \"arriveNokBurned()\"> Leave the city </button><button onClick = \"construction()\"> Walk to Construction Site </button>";
 }
 
 function construction(){
-    document.body.innerHTML = "You see a group of builders building a store. <button onClick = \"nokEnter()\">Leave</button>";
+    document.body.innerHTML = "You see a group of builders building a store. <button onClick = \"nokEnter()\"> Leave </button>";
 }
 
 
 function nokLeave() {
     //Executes travel code in Travel.js
     clear();
-    document.body.innerHTML = document.body.innerHTML + "<button onClick = \"travelJudalSet()\">Judal</button><button onClick = \"travelArgo()\">Argo</button><button onClick = \"arriveNokBurned()\">Stay</button>";
+    document.body.innerHTML = document.body.innerHTML + "<button onClick = \"travelJudalSet()\"> Judal </button><button onClick = \"travelArgo()\"> Argo </button><button onClick = \"arriveNokBurned()\"> Stay </button>";
 };
