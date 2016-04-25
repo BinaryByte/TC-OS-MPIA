b62ab3492375671f5ff2293d675d15ec5ae3bc51
Tyly04 <tyly04@gmail.com>
2016-04-25 15:04:09 -0700

    Update Travel.js

--- JS/Systems/Travel.js
+++ JS/Systems/Travel.js
@@ -346,7 +346,7 @@ function magicCaveEvent(){
 }
 
 function treasureChance(){
-    var chance = Math.floor(Math.random() * 100) * goldChance;
+    var chance = Math.floor(Math.random() * (level * 10)) * goldChance;
     window.alert("You\'ve found " + chance + " gold!");
     gold = gold + chance;
     travelNok();
