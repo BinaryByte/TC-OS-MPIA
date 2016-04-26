e4bba6440b5e9b83140de2c808f5dfe221062ce2
Tyly04 <tyly04@gmail.com>
2016-04-26 08:53:08 -0700

    Update Argo.js

--- JS/Towns/Argo.js
+++ JS/Towns/Argo.js
@@ -259,6 +259,7 @@ function prayGold(){
     health=maxHealth;
     edit("You feel divine providence shining upon you. <input type=\"button\" onClick=\"again()\" value=\"leave\"></input>");
     } else {
+    clear();
     edit("You can't afford to give a gold offering. <input type = \"button\" onClick = \"again()\" value = \"leave\"</input>");
     }
 }
