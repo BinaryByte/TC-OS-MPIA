4a4b6da1e5ab4dd17f7829fa3f45edab7cccb19a
Nis <niscooln@gmail.com>
2016-04-27 08:42:19 -0700

    Update Ruilind.js

--- JS/Towns/Ruilind.js
+++ JS/Towns/Ruilind.js
@@ -1,1 +1,8 @@
-//Don't put anything here yet noah
+arriveRuillind(){
+  clear();
+  edit("You are now in the port city of Ruillind! <input value=\" Explore \" type=\"button\" onClick=\"exploreRuillind()\"></input> <input value=\" Leave \" type=\"button\" onClick=\"\"></input>");
+}
+exploreRuillind(){
+  
+}
+//boss battle
